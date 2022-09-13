1c8a65e6a72613de8187038ed1414d16c603911d
Ale200283 <ale_afon@hotmail.com>
2022-09-13 12:45:55 -0300

    arreglo de skill y componentes de angular

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { InformacionpersonalComponent } from './componentes/informacionpersonal/informacionpersonal.component';
+import { ExperiencialaboralComponent } from './componentes/experiencialaboral/experiencialaboral.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { HardandsoftskillComponent } from './componentes/hardandsoftskill/hardandsoftskill.component';
+import { ProyectoComponent } from './componentes/proyecto/proyecto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BannerComponent,
+    InformacionpersonalComponent,
+    ExperiencialaboralComponent,
+    EducacionComponent,
+    HardandsoftskillComponent,
+    ProyectoComponent
   ],
   imports: [
     BrowserModule
---
+++ portfolio/src/app/componentes/informacionpersonal/informacionpersonal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-informacionpersonal',
+  templateUrl: './informacionpersonal.component.html',
+  styleUrls: ['./informacionpersonal.component.css']
+})
+export class InformacionpersonalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
